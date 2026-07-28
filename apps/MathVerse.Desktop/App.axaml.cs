4807cf55f9cc83d8315aeb4a9ba5bcba4be62fa1
darkfandom <astriphysics@gmail.com>
2026-07-28 13:50:13 +0530

    Removed The Currrent Frontend Completely

--- apps/MathVerse.Desktop/App.axaml.cs
+++ apps/MathVerse.Desktop/App.axaml.cs
@@ -1,17 +1,11 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using MathVerse.Desktop.ViewModels;
-using MathVerse.Desktop.Views;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace MathVerse.Desktop;
 
 public class App : Application
 {
-    private static IHost? _appHost;
-
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -19,18 +13,9 @@ public class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        _appHost = Host.CreateDefaultBuilder()
-            .ConfigureServices((_, services) =>
-            {
-                services.AddSingleton<GraphViewModel>();
-                services.AddSingleton<WorkspaceViewModel>();
-            })
-            .Build();
-
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            var workspace = _appHost.Services.GetRequiredService<WorkspaceViewModel>();
-            desktop.MainWindow = new MainWindow { DataContext = workspace };
+            desktop.MainWindow = new MainWindow();
         }
 
         base.OnFrameworkInitializationCompleted();
--- apps/MathVerse.Desktop/Views/MainWindow.cs
+++ apps/MathVerse.Desktop/Views/MainWindow.cs
@@ -1,14 +1,6 @@
-using System;
-using System.Globalization;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Data.Converters;
-using Avalonia.Input;
-using Avalonia.Interactivity;
-using Avalonia.Media;
-using MathVerse.Desktop.ViewModels;
 
-namespace MathVerse.Desktop.Views;
+namespace MathVerse.Desktop;
 
 public partial class MainWindow : Window
 {
@@ -16,101 +8,4 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
-
-    private void Window_KeyDown(object? sender, KeyEventArgs e)
-    {
-        if (DataContext is not WorkspaceViewModel vm) return;
-
-        if (e.Key == Key.Enter && e.KeyModifiers.HasFlag(KeyModifiers.Control))
-        {
-            vm.QuickEvaluateCommand.Execute(null);
-            e.Handled = true;
-        }
-        else if (e.Key == Key.Enter && !e.KeyModifiers.HasFlag(KeyModifiers.Control))
-        {
-            if (FocusManager?.GetFocusedElement() is TextBox tb && tb.Name == "ExpressionInput")
-            {
-                vm.AddGraphFromExpressionCommand.Execute(null);
-                e.Handled = true;
-            }
-        }
-        else if (e.Key == Key.Delete && vm.Graph.SelectedGraph != null)
-        {
-            vm.Graph.RemoveGraphCommand.Execute(vm.Graph.SelectedGraph);
-            e.Handled = true;
-        }
-        else if (e.Key == Key.G && e.KeyModifiers.HasFlag(KeyModifiers.Control))
-        {
-            vm.Graph.ToggleGridCommand.Execute(null);
-            e.Handled = true;
-        }
-        else if (e.Key == Key.F && e.KeyModifiers.HasFlag(KeyModifiers.Control))
-        {
-            vm.Graph.FitAllCommand.Execute(null);
-            e.Handled = true;
-        }
-        else if (e.Key == Key.H && !e.KeyModifiers.HasFlag(KeyModifiers.Control))
-        {
-            vm.Graph.HomeCommand.Execute(null);
-            e.Handled = true;
-        }
-    }
-
-    private void ExpressionInput_KeyDown(object? sender, KeyEventArgs e)
-    {
-        if (DataContext is not WorkspaceViewModel vm) return;
-
-        if (e.Key == Key.Enter && e.KeyModifiers.HasFlag(KeyModifiers.Control))
-        {
-            vm.QuickEvaluateCommand.Execute(null);
-            e.Handled = true;
-        }
-        else if (e.Key == Key.Enter)
-        {
-            vm.AddGraphFromExpressionCommand.Execute(null);
-            e.Handled = true;
-        }
-    }
-
-    private void Toggle3D_Click(object? sender, RoutedEventArgs e)
-    {
-        if (DataContext is WorkspaceViewModel vm)
-            vm.Graph.Is3D = !vm.Graph.Is3D;
-    }
-
-    private void ExplorerItem_Tapped(object? sender, TappedEventArgs e)
-    {
-        if (sender is Control control && control.Tag is GraphEntry entry && DataContext is WorkspaceViewModel vm)
-            vm.Graph.SelectedGraph = entry;
-    }
-
-    private void ExplorerDelete_Tapped(object? sender, TappedEventArgs e)
-    {
-        if (sender is Control control && control.Tag is GraphEntry entry && DataContext is WorkspaceViewModel vm)
-            vm.Graph.RemoveGraphCommand.Execute(entry);
-        e.Handled = true;
-    }
-
-    private void HistoryItem_Tapped(object? sender, TappedEventArgs e)
-    {
-        if (sender is Control control && control.Tag is string expr && DataContext is WorkspaceViewModel vm)
-            vm.ExpressionInput = expr;
-    }
-}
-
-public class ErrorToBrushConverter : IValueConverter
-{
-    public static readonly ErrorToBrushConverter Instance = new();
-
-    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        if (value is bool isError)
-            return isError
-                ? new SolidColorBrush(Color.Parse("#FF4444"))
-                : new SolidColorBrush(Color.Parse("#E8E8F0"));
-        return new SolidColorBrush(Color.Parse("#E8E8F0"));
-    }
-
-    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-        => throw new NotSupportedException();
 }
